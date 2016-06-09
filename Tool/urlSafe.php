04eb9d453cd3b43195026fe15bdee9420b2e01de
Александр Нежинский <xfilee@gmail.com>
2016-06-10 01:25:56 +0300

    Добавлено пояснение.

--- Tool/urlSafe.php
+++ Tool/urlSafe.php
@@ -6,4 +6,19 @@ trait urlSafe
 	protected function urlSafeB64Encode($data){return str_replace(['+', '/', '\r', '\n', '='], ['-', '_'], base64_encode($data));}
 	protected function urlSafeB64Decode($b64){return base64_decode(str_replace( ['-', '_'], ['+', '/'], $b64));}	
 }
+
+/*
+Example:
+
+class ClassName
+{
+	use X\Tool\urlSafe;
+
+	public function Test($Data)
+	{
+		return $this->urlSafeB64Encode($Data);
+	}
+}
+
+*/
 ?>
