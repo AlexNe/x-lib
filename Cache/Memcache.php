2161b02d242337df8dc7af738e6104b9e92503ac
Александр Нежинский <xfilee@gmail.com>
2016-06-25 17:33:29 +0300

    Проверка Мемкеш

--- Cache/Memcache.php
+++ Cache/Memcache.php
@@ -1,28 +1,67 @@
 <?php
 namespace X\Cache;
 
-class Memcache extends \memcache
+class Memcache
 {
+	/**
+	 * @var mixed
+	 */
 	protected $compression = false;
+	/**
+	 * @var mixed
+	 */
 	protected $prefix;
+	/**
+	 * @var mixed
+	 */
 	protected $mcache = false;
-	function __construct( $prefix, $host = "localhost", $port = 11211, $compression = false )
+	/**
+	 * @param $prefix
+	 * @param $host
+	 * @param $port
+	 * @param $compression
+	 */
+	public function __construct($prefix, $host = "localhost", $port = 11211, $compression = false)
 	{
-		$this->prefix = $prefix;
-		$this->compression = $compression;
-		$this->connect($host,$port);
+		if (class_exists("\memcache"))
+		{
+			$this->prefix      = $prefix;
+			$this->compression = $compression;
+			$this->mcache      = new \memcache();
+
+			$this->mcache->connect($host, $port)
+			|| $this->mcache = false;
+		}
 	}
 
-	public function set( $key, $value, $expire = 240 )
+	/**
+	 * @param $key
+	 * @param $value
+	 * @param $expire
+	 */
+	public function set($key, $value, $expire = 240)
 	{
+		if ( ! $this->mcache)
+		{
+			return $this->mcache;
+		}
+
 		$key = $this->prefix . $key;
-		return parent::set( md5($key), $value, $this->compression, $expire);
+		return $this->mcache->set(md5($key), $value, $this->compression, $expire);
 	}
 
+	/**
+	 * @param $key
+	 */
 	public function get($key)
 	{
+		if ( ! $this->mcache)
+		{
+			return $this->mcache;
+		}
+
 		$key = $this->prefix . $key;
-		return parent::get( md5($key), $this->compression);
+		return $this->mcache->get(md5($key), $this->compression);
 	}
 }
 ?>
