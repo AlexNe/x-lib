8376f49fd0254dba58bdcffd06237ae0d9b2e012
Александр Нежинский <xfilee@gmail.com>
2016-07-19 21:58:54 +0300

    Тулзы преобразования

---
+++ Tool/BC/BcHexDec.php
@@ -0,0 +1,19 @@
+<?php
+namespace X\Tool\BC;
+
+trait BcHexDec {
+/**
+ * @param $hex
+ */
+	protected function bchexdec($hex) {
+		if (strlen($hex) == 1) {
+			return hexdec($hex);
+		} else {
+			$remain = substr($hex, 0, -1);
+			$last   = substr($hex, -1);
+			return bcadd(bcmul(16, bchexdec($remain)), hexdec($last));
+		}
+	}
+}
+
+?>
